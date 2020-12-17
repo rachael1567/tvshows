a7ca4833320721de9eaf1927725ed44968416519
yet <yet@Amins-iMac-3.local>
2020-12-17 15:45:43 +0000

    first commit

---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,14 @@
+const Aboutpage = () => {
+    return (
+      <div className="about">
+        <h3 className="about__title">ABOUT PAGE</h3>
+        <div className="about__info">
+          <p>Tv Show Search App (v1.0)</p>
+          <div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Aboutpage;
